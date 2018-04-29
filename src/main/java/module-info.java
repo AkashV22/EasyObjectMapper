42ddd2d5f149f195faff14bbeb85be69d6c88fc5
AkashV22 <iakashv22@gmail.com>
2018-04-29 19:06:09 +0100

    Added support for Java 9 modules with an explicit module-info.java file, while ensuring that the code is still usable in Java 8.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * Simple Java object-to-object mapping without reflection.
+ */
+@SuppressWarnings("JavaModuleNaming")
+module com.theakashv22.util.easy.object.mapper {
+    exports com.theakashv22.util.easyobjectmapper;
+}
